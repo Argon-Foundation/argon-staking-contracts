b54216a69179da155ea7ed248251143feec92f7b
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2021-06-11 16:34:01 +0300

    Initial commit

---
+++ ArgonStakeMaster.sol
@@ -0,0 +1,66 @@
+pragma solidity 0.6.12;
+
+import "ArgonStakingPool.sol";
+
+contract ArgonStakeMaster is Ownable {
+    address[] public stakingPools;
+    event PoolCreated(
+        address owner,
+        address newPoolAddress,
+        address stakingToken,
+        address poolToken,
+        uint256 startBlock,
+        uint256 finishBLock,
+        uint256 poolTokenAmount,
+        bool isPenalty,
+        uint256 penaltyRate,
+        address penaltyAddress,
+        uint256 penaltyBlockLength
+    );
+
+    function createStakingPool(
+        IERC20 _stakingToken,
+        IERC20 _poolToken,
+        uint256 _startBlock,
+        uint256 _finishBlock,
+        uint256 _poolTokenAmount,
+        bool _isPenalty,
+        uint256 _penaltyRate,
+        address _penaltyAddress,
+        uint256 _penaltyBlockLength
+    ) public onlyOwner {
+        ArgonStakingPool newStakingPool =
+            new ArgonStakingPool(
+                _stakingToken,
+                _poolToken,
+                _startBlock,
+                _finishBlock,
+                _poolTokenAmount,
+                _isPenalty,
+                _penaltyRate,
+                _penaltyAddress,
+                _penaltyBlockLength
+            );
+        newStakingPool.transferOwnership(msg.sender);
+        _poolToken.transferFrom(
+            msg.sender,
+            address(newStakingPool),
+            _poolTokenAmount
+        );
+        stakingPools.push(address(newStakingPool));
+
+        emit PoolCreated(
+            msg.sender,
+            address(newStakingPool),
+            address(_stakingToken),
+            address(_poolToken),
+            _startBlock,
+            _finishBlock,
+            _poolTokenAmount,
+            _isPenalty,
+            _penaltyRate,
+            _penaltyAddress,
+            _penaltyBlockLength
+        );
+    }
+}
